495e5b70311241049f1a0ca0f41cb8c84a3389ae
syabin <syabin@users.noreply.github.com>
2026-06-15 21:52:17 +0800

    增加自更新

--- update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo   GitHub Manager 强制更新工具
-echo ========================================
-echo.
-echo 正在更新 github-manager...
-echo.
-python "%~dp0update.py"
-pause
